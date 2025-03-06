12611662db1450c7feb22b9b293af07cf7fd5133
naveenjha031 <naveenjha344@gmail.com>
2025-03-06 16:09:01 +0530

    [Naveen] Add : Connecting to database

--- src/main/java/com/bridgelabz/addressbookapp/model/AddressBookModel.java
+++ src/main/java/com/bridgelabz/addressbookapp/model/AddressBookModel.java
@@ -1,4 +1,7 @@
 package com.bridgelabz.addressbookapp.model;
+
+import jakarta.persistence.*;
+
 import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
@@ -6,8 +9,11 @@ import lombok.NoArgsConstructor;
 @Data  // Generates getters, setters, toString, equals, and hashCode
 @AllArgsConstructor  // Generates a constructor with all fields
 @NoArgsConstructor   // Generates a default constructor
+@Entity
+@Table(name = "address_book")
 public class AddressBookModel {
-
-    private int id;
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private int id;
     private String name;
 }
